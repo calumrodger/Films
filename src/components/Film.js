831db45f40e41f9b8566097a79773c657f9624ba
calumrodger <94742384+calumrodger@users.noreply.github.com>
2022-02-14 15:39:48 +0000

    hw complete

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,9 +4,7 @@ const Film = ({id, name, url}) => {
 
     return(
         <>
-        <p>{id}</p>
-        <h2>{name}</h2>
-        <h3><a href={url}>Find out more at IMDB</a></h3>
+        <h2><a href={url}>{name}</a></h2>
         </>
     )
 }
